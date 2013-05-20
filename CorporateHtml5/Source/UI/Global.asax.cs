3210b8e06f149b9185c5e93af418b6f4d059b0dd
Colin Bowern <colin@bowern.com>
2013-05-19 21:54:00 -0400

    Adding two of four DevTeach decks

---
+++ CorporateHtml5/Source/UI/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Routing;
+
+namespace CorporateHtml5
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.MapHubs();
+        }
+    }
+}
---
+++ CorporateHtml5/packages/Modernizr.full.2.6.2.1/content/App_Readme/modernizr.full/readme.txt
@@ -0,0 +1,4 @@
+This is a nearly full version of Modernizr 2.6.2 that I downloaded from
+http://modernizr.com/download.   It includes everying  except added css
+queries and the  community  add-ons,  including yepnope support so that
+Modernizr.load will work.
